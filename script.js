d6f59a6402906c429ef8baa3776d9fe21b64d764
sankalp-78 <137093387+sankalp-78@users.noreply.github.com>
2023-07-22 21:54:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const themeToggle = document.getElementById('toggle-theme');
-const body = document.body;
-
-themeToggle.addEventListener('change', () => {
-  body.classList.toggle('dark-theme');
-});
